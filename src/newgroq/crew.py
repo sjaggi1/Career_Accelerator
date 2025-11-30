0cb77d6e7343513950078cae353c29aaee8840a9
sjaggi1 <144943702+sjaggi1@users.noreply.github.com>
2025-11-30 17:57:15 +0530

    Update crew.py

--- src/newgroq/crew.py
+++ src/newgroq/crew.py
@@ -2,6 +2,7 @@ from crewai import Agent, Crew, Process, Task
 from crewai.project import CrewBase, agent, crew, task
 from crewai.agents.agent_builder.base_agent import BaseAgent
 from typing import List
+import os
 
 @CrewBase
 class Newgroq():
